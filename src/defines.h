c12782e52b00ceda996f435a9707b77ae191f79b
Naum Azeredo <naumazeredo@gmail.com>
2015-01-05 10:15:14 -0300

    Changed predefined types (e.g. uint32) to use SDL types

--- src/defines.h
+++ src/defines.h
@@ -4,11 +4,19 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+using int8 = Sint8;
+using int16 = Sint16;
+using int32 = Sint32;
+using int64 = Sint64;
+
 using uint8 = Uint8;
 using uint16 = Uint16;
 using uint32 = Uint32;
 using uint64 = Uint64;
 
-using bool32 = uint32;
+//using bool32 = uint32;
+
+//using real32 = float;
+//using real64 = double;
 
 #endif
